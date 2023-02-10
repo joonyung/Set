e8067854e035fce88bbac4be431a2ac39302ff78
Junyeong Lee <joonyung2@gmail.com>
2023-02-10 15:14:19 +0900

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by JY on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
